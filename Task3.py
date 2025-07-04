32697fe3877df11f3be32b0372555c4277d6a49c
karankumar58 <135383885+karankumar58@users.noreply.github.com>
2025-07-04 12:41:36 +0500

    Delete Task3.py

--- Task3.py
+++
@@ -1,9 +0,0 @@
-a = float(input("Enter number one: "))
-b = float(input("Enter number two: "))
-c = float(input("Enter number three: "))
-
-largest = max(a, b, c)
-smallest = min(a, b, c)
-
-print(f"The largest number is: {largest}")
-print(f"The smallest number is: {smallest}")
